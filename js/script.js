43bc7b9322ea3c0f2ad5b70f205f08bc5caf325a
padmajapinnika <padmajapinnika@gmail.com>
2025-02-13 15:46:42 -0500

    updates of js and css for names.
    adding some colors to pokemon names and displaying pokemon names on browser with some special pokemon.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-let pokemonList=[
-    {
-        name:"Bulbasur",
-        height:7,
-        types:["grass","poison"]
-    },
-    {
-        name:"Ivysaur",
-        height:1,
-        types:["grass","poison"]
-    },
-    {
-        name:"venusaur",
-        height:2,
-        types:["grass","poison"]
-    }
+let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Nidoking", height: 1.4, types: ["ground", "poison"] },
+    { name: "Charmander", height: 0.6, types: ["fire"] }
 ];
+//displaying pokemon names with heights with for loop
+for (let i = 0; i < pokemonList.length; i++) {
+    let output = "<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")";
+    //finding special pokemon
+    if(pokemonList[i].height<0.7){
+        output += " - Aww! It's so cute";
+
+    }
+    output+="</p>"
+document.write(output);
+}
